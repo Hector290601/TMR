47fbff67e86cd4dd33b5316db7e53a7637030877
HectorRobles <robletes062901@gmail.com>
2020-09-14 13:16:44 -0500

    arduinoSerial/

--- mio/arduinoSerial/directionTest/directionTest.ino
+++ mio/arduinoSerial/directionTest/directionTest.ino
@@ -1,19 +1,50 @@
 #include <Servo.h>
 
+int lPwm = 5;
+int rPwm = 6;
+int enableMotor = 8;
 Servo directionCar;
 int directionDegree;
 
 void setup () {
-  directionCar.attach(2);
-  Serial.begin(9600);
+  directionCar.attach(9);
   directionCar.write(90);
+  pinMode(lPwm, OUTPUT);
+  pinMode(rPwm, OUTPUT);
+  pinMode(enableMotor, OUTPUT);
+  digitalWrite(lPwm, 0);
+  digitalWrite(rPwm, 0);
+  digitalWrite(enableMotor, 0);
+  Serial.begin(9600);
 }
  
 void loop(){
- if (Serial.available() > 0) {
-  directionDegree = Serial.parseInt();
-//  if(directionDegree >= 60 and directionDegree <= 120){
-    directionCar.write(directionDegree);
-//  }
+ if(Serial.available() > 0){
+   directionDegree = Serial.parseInt();
+   if(directionDegree != 255){
+     directionCar.write(directionDegree);
+   }else{
+     directionCar.write(90);
+   }
+ }
+ if(directionDegree == 90){
+   digitalWrite(enableMotor, 1);
+   analogWrite(lPwm, 20);
+   analogWrite(rPwm, 0);
+   Serial.print(directionDegree);
+ }else if(directionDegree == 60 || directionDegree == 120){
+   digitalWrite(enableMotor, 1);
+   analogWrite(lPwm, 20);
+   analogWrite(rPwm, 0);
+   Serial.print(directionDegree);
+ }else if(directionDegree == 255){
+   analogWrite(lPwm, 0);
+   analogWrite(rPwm, 0);
+   Serial.print(directionDegree);
+ }else{
+   analogWrite(lPwm, 0);
+   analogWrite(rPwm, 30);
+   digitalWrite(enableMotor, 0);
+   Serial.print(directionDegree);
  }
 }
