a977c9b9daf4aefa691baa8c63ae50bd26cbc6c1
Héctor Robles <33700655+Hector290601@users.noreply.github.com>
2020-10-10 14:16:03 -0500

    Update xfceOpenCvRos.bash

--- xfceOpenCvRos.bash
+++ xfceOpenCvRos.bash
@@ -1,73 +1,73 @@
 #!/bin/bash
 echo "Empezando!"
-sudo apt-get update
-sudo apt autoremove
-sudo apt upgrade
+apt-get update
+apt autoremove
+apt upgrade
 ##################################################SSH
 echo "Instalando y habilitando SSH"
-sudo apt install openssh-server
-sudo systemctl enable openssh-server
-sudo systemctl start openssh-server
-sudo apt-get update
-sudo apt autoremove
-sudo apt upgrade
+apt install openssh-server
+systemctl enable openssh-server
+systemctl start openssh-server
+apt-get update
+apt autoremove
+apt upgrade
 #################################################GUI
 echo "Instalando escritorio Xfce"
-sudo apt install tasksel
-sudo apt-get install xubuntu-desktop
-sudo apt-get update
-sudo apt autoremove
-sudo apt upgrade
+apt install tasksel
+apt-get install xubuntu-desktop
+apt-get update
+apt autoremove
+apt upgrade
 ################################################Java
 echo "Instalando Java"
-sudo apt install default-jre
-sudo apt install default-jdk
+apt install default-jre
+apt install default-jdk
 #################################################OpenCv
 ########################Cmake
 echo "Instalando cmake"
-sudo apt-get install cmake
+apt-get install cmake
 ########################gcc y g++
 echo "Instalando gcc y g++"
-sudo apt-get install gcc g++
+apt-get install gcc g++
 ########################Dependencias python 2 y 3
 echo "Instalando dependencias python"
-sudo apt-get install python-dev python-numpy
-sudo apt-get install python3-dev python3-numpy
+apt-get install python-dev python-numpy
+apt-get install python3-dev python3-numpy
 ########################Dependencias Opencv
 echo "Instalando "
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
-sudo apt-get install libgtk2.0-dev
-sudo apt-get install libgtk-3-dev
-sudo apt-get install libpng-dev
-sudo apt-get install libjpeg-dev
-sudo apt-get install libopenexr-dev
-sudo apt-get install libtiff-dev
-sudo apt-get install libwebp-dev
-sudo apt-get install libavresample-dev
+apt-get install libavcodec-dev libavformat-dev libswscale-dev
+apt-get install libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+apt-get install libgtk2.0-dev
+apt-get install libgtk-3-dev
+apt-get install libpng-dev
+apt-get install libjpeg-dev
+apt-get install libopenexr-dev
+apt-get install libtiff-dev
+apt-get install libwebp-dev
+apt-get install libavresample-dev
 echo "Instalando git"
-sudo apt-get install git
+apt-get install git
 echo "Clonando repositorio de Opencv"
 git clone https://github.com/opencv/opencv.git
 mkdir build
 cd build
 echo "Creando archivo swap"
-sudo dd if=/dev/zero of=/swapfile bs=64M count=16
-sudo mkswap /swapfile
-sudo swapon /swapfile
+dd if=/dev/zero of=/swapfile bs=64M count=16
+mkswap /swapfile
+swapon /swapfile
 echo "Compilando openCv"
 cmake ../ -DENABLE_PRECOMMPILED_HEADERS=OFF
 make
 echo "Instalando compilado de OpenCv"
-sudo make install
+make install
 #################################################ROS
 echo "Instalando ROS"
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
-sudo apt update
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | apt-key add -
+apt update
 echo "Instalando ROS Noetic Desktop"
-sudo apt install ros-noetic-desktop
+apt install ros-noetic-desktop
 source /opt/ros/noetic/setup.bash
 source /opt/ros/noetic/setup.bash
 source ~/.bashrc
@@ -85,16 +85,16 @@ echo "Instalando pip para python3"
 python3 -m pip install -U pip
 ##################################################Instalar pandas
 echo "Instalando Pandas"
-sudo apt-get install python3-pandas
-sudo pip3 install hypothesis
+apt-get install python3-pandas
+pip3 install hypothesis
 ##################################################Instalar matplotlib
 echo "Instalando Matplotlib"
 python3 -m pip install -U matplotlib
-sudo apt install libfreetype6-dev
-sudo apt install pkg-config
+apt install libfreetype6-dev
+apt install pkg-config
 ##################################################Instalar VIM
 echo "Instalando Vim"
-sudo apt-get install vim
+apt-get install vim
 ##################################################Ultimo Paso
-sudo apt update
-sudo apt upgrade
+apt update
+apt upgrade
