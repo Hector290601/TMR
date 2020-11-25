4293df2f153e1ab4e7b262b3ecbeea62d889ee5c
Hector290601 <robletes062901@gmail.com>
2020-11-24 19:24:00 -0600

    AutoModelCarRulebook.pdf

--- Hector/cpp/videoCapture/main.cpp
+++ Hector/cpp/videoCapture/main.cpp
@@ -9,14 +9,14 @@ int main(int, char**)
 {
     Mat frame;
     //--- INITIALIZE VIDEOCAPTURE
-    VideoCapture cap;
+    VideoCapture cap(0);
     // open the default camera using default API
     // cap.open(0);
     // OR advance usage: select any API backend
-    int deviceID = 0;             // 0 = open default camera
-    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+    //int deviceID = 11;             // 0 = open default camera
+    //int apiID = cv::CAP_ANY;      // 0 = autodetect default API
     // open selected camera using selected API
-    cap.open(deviceID, apiID);
+    //cap.open(deviceID, apiID);
     // check if we succeeded
     if (!cap.isOpened()) {
         cerr << "ERROR! Unable to open camera\n";
