4b2fcca195b7f95e083a3f488475a1cfda1e6b8a
HectorRobles <robletes062901@gmail.com>
2020-11-25 16:19:28 -0600

    AutoModelCarRulebook.pdf

--- Hector/python/kinectSensor/led.py
+++ Hector/python/kinectSensor/led.py
@@ -2,7 +2,7 @@ import freenect
 import time
 
 dev = freenect.open_device(freenect.init(), 0)
-for i in range(0, 31):
+for i in [0, 1, 2, 3, 4, 5, 6]:
+    freenect.set_led(dev, i)
     print(i)
-    freenect.set_tilt_degs(dev, i)
-    time.sleep(0.5)
+    time.sleep(3)
