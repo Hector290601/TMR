d4593ba5a45c07b063850bc178fc77f0725ffa3a
HectorRobles <robletes062901@gmail.com>
2020-12-11 15:02:23 -0600

    AutoModelCarRulebook.pdf

--- Hector/python/laneDetection/kinect.py
+++ Hector/python/laneDetection/kinect.py
@@ -21,13 +21,13 @@ def cannyImage(cannyGray):
     cannyCanny = cv2.Canny(cannyBlur, 10, 250)
     return cannyCanny
 
-def regionOfInterest(regionImage):
-    #regionPolygons = np.array([[(0, 448), (639, 425), (434, 276), (256, 159)]])
-    regionPolygons = np.array([[(0, 400), (640, 400), (640, 180), (0, 180)]])
+def regionOfInterest(regionImage, toPol):
+    regionPolygons = np.array([[(0, 470), (640, 470), (640, 420), (400, 250), (220, 250), (0, 420)]])
+    cv2.polylines(toPol,[regionPolygons],True,(0,255,255))
     zeros = np.zeros_like(regionImage)
     cv2.fillPoly(zeros, regionPolygons, 255)
     regionedImage = cv2.bitwise_and(regionImage, regionImage, mask=zeros)
-    return regionedImage
+    return regionedImage, toPol
 
 def colorRange(colorFrame, colorGray, colorCropped):
     colorMax = np.array([255, 255, 255])
@@ -47,120 +47,97 @@ def averagedSlopeIntercept(interceptImage, interceptLines):
         intercept = parameters[1]
         if slope < 0:
             leftFit.append((slope, intercept))
-            #leftX.append(slope)
-            #leftY.append(intercept)
         else:
             rightFit.append((slope, intercept))
-            #rightX.append(slope)
-            #rightY.append(intercept)
     leftFitAverage = np.average(leftFit, axis = 0)
     rightFitAverage = np.average(rightFit, axis = 0)
     try:
         isNanLeft = math.isnan(leftFitAverage[0])
     except:
         isNanLeft = math.isnan(leftFitAverage)
-    #print("Left ", isNanLeft)
     try:
         isNanRight = math.isnan(rightFitAverage[0])
     except:
         isNanRight = math.isnan(rightFitAverage)
-    #print("Right ", isNanRight)
     leftLine, slopeLeft, interceptLeft =  makeCoordinates(interceptImage, leftFitAverage)
     rightLine, slopeRight, interceptRight = makeCoordinates(interceptImage, rightFitAverage)
     deltaTime = time.time() - startTime
     if isNanLeft:
         print('L')
-        #detected.loc[len(detected)]=['L', leftFitAverage, rightFitAverage, leftLine, rightLine, slopeLeft, interceptLeft, slopeRight, interceptRight]
     elif isNanRight:
         print('R')
-        #detected.loc[len(detected)]=['R', leftFitAverage, rightFitAverage, leftLine, rightLine, slopeLeft, interceptLeft, slopeRight, interceptRight]
     else:
         detected.loc[len(detected)]=['C', leftFitAverage, rightFitAverage, leftLine, rightLine, slopeLeft, interceptLeft, slopeRight, interceptRight]
     return np.array([leftLine, rightLine]), isNanLeft, isNanRight
 
 def makeCoordinates(coordinatesImage, lineParameters):
-    #print("lineParameters: ", lineParameters)
     try:
-        slope, intercept = lineParameters #intercept es la ordenada al origen de la recta
-        #print("slope: ", slope, "intercept ", intercept)
-        y1 = coordinatesImage.shape[0] #obtiene el valor '0' de la imagen
-        y2 = int(y1*(3/5)) #esto es pra determinar el largo de la recta que dibuja en la imagen
-        x1 = int((y1 - intercept)/slope) #obtiene la ecuación de la recta con el primer punto
-        x2 = int((y2 - intercept) / slope) # obtiene la ecuación de la recta con el segundo punto
-        #print(type(lineParameters))
+        slope, intercept = lineParameters
+        y1 = coordinatesImage.shape[0]
+        y2 = int(y1*(3/5))
+        x1 = int((y1 - intercept)/slope)
+        x2 = int((y2 - intercept) / slope)
         return np.array([x1, y1, x2, y2]), slope, intercept
     except:
         return np.array([0, 0, 0, 0]), np.nan, np.nan
 
 def displayLines(displayImage, displayLinesVar):
-    lineImage = np.zeros_like(displayImage)#obtiene un arreglo de ceros de las mismas características de la imagen
-    if displayLinesVar is not None: #verifica que existan líneas
-        for x1, y1, x2, y2 in displayLinesVar: #itera en cada línea
-            #x1, y1, x2, y2 = line.shape()
-            #print(lines.shape, 'Shapes on the line')
-            #print(lines, 'lines')
-            #print(x1, 'x1', y1, 'y1', x2, 'x2', y2, 'y2')
-            try: #evita que el programa falle si no hay líneas
-                cv2.line(lineImage, (x1, y1), (x2, y2), (255, 255, 255), 3) #dibuja una línea azul en las líneas
+    lineImage = np.zeros_like(displayImage)
+    if displayLinesVar is not None:
+        for x1, y1, x2, y2 in displayLinesVar:
+            try:
+                cv2.line(lineImage, (x1, y1), (x2, y2), (255, 255, 255), 3)
             except:
-                lineImage = lineImage #regresa una matriz de ceros
+                lineImage = lineImage
     return lineImage
 
+def improveImage(frame, a):
+    hsvVideo = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsvVideo = hsvVideo.astype(np.uint8)
+    h, s, v = cv2.split(hsvVideo)
+    v += a
+    hsvVideo = cv2.merge((h, s, v))
+    return hsvVideo
+
 if __name__ == '__main__':
-    try:
-        #cap = cv2.VideoCapture(0)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        while True:
-            #os.system("clear")
-            #ret, origFrame = cap.read()
-            #origFrame, _ = freenect.sync_get_video(0, freenect.VIDEO_IR_10BIT)
-            origFrame, _ = freenect.sync_get_video()
-            #np.clip(origFrame, 0, 2**10-1, origFrame)
-            #origFrame >>=2
-            #origFrame = origFrame.astype(np.uint8)
-            origFrame = cv2.cvtColor(origFrame,cv2.COLOR_RGB2BGR)
-            coppiedFrame = np.copy(origFrame)
-            hsvFrame = cv2.cvtColor(origFrame,cv2.COLOR_BGR2HSV)
-            origFrame = hsvFrame
-            gray = cv2.cvtColor(origFrame, cv2.COLOR_BGR2GRAY)
-            canny = cannyImage(gray)
-            croppedImage = regionOfInterest(canny)
-            colorInRange = colorRange(coppiedFrame, gray, croppedImage)
-            lines = cv2.HoughLinesP(colorInRange, 1, np.pi/180, 50, minLineLength=20, maxLineGap=50)
-            #print(lines)
-            #left = range(0, 480 // 2)
-            #right = range(480 // 2, 480)
-            if lines is not None:
-                averagedLines, left, right =  averagedSlopeIntercept(coppiedFrame, lines)
-                if left:
-                    cv2.putText(coppiedFrame, '<---', (0, 30), font, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
-                elif right:
-                    cv2.putText(coppiedFrame, '--->', (0, 30), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-                else:
-                    cv2.putText(coppiedFrame, '^', (0, 30), font, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
-                timeText = time.strftime("%Y/%m/%d %H:%M:%S %Z", time.localtime())
-                cv2.putText(coppiedFrame, timeText, (250, 20), font, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
-                lineImage = displayLines(coppiedFrame, averagedLines)
-                for line in lines:
-                    x1, y1, x2, y2 = line[0]
-                    #cv2.rectangle(coppiedFrame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-                    cv2.line(coppiedFrame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-            if cv2.waitKey(25) & 0xFF == ord('s'):
-                break
-            #cv2.imshow("origFrame", origFrame)
-            #salida.write(coppiedFrame)
-            cv2.imshow("coppiedFrameWithLines", coppiedFrame)
-            #if lines is not None:
-            #    cv2.imshow("lineImage", lineImage)
-            
-            #cv2.imshow("lines", lines)
-        #print(detected)
-        timeSaved = time.strftime("%Y%m%d%H%M%S%Z", time.localtime())
-        name1 = 'detected' + timeSaved + '.csv'
-        name2 = 'detected' + timeSaved + 'HeadAndIndexFalse.csv'
-        detected.to_csv(name1)
-        detected.to_csv(name2, header=False, index=False)
-        cap.release()
-        cv2.destroyAllWindows()
-    except:
-        print("Algo malo ha ocurrido!")
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    vChange = 0
+    while True:
+        origFrame, _ = freenect.sync_get_video()
+        origFrame = cv2.cvtColor(origFrame,cv2.COLOR_RGB2BGR)
+        coppiedFrame = np.copy(origFrame)
+        origFrame = improveImage(origFrame, vChange)
+        gray = cv2.cvtColor(origFrame, cv2.COLOR_BGR2GRAY)
+        canny = cannyImage(gray)
+        croppedImage, origFrame = regionOfInterest(canny, origFrame)
+        colorInRange = colorRange(coppiedFrame, gray, croppedImage)
+        lines = cv2.HoughLinesP(colorInRange, 1, np.pi/180, 50, minLineLength=20, maxLineGap=50)
+        if lines is not None:
+            averagedLines, left, right =  averagedSlopeIntercept(coppiedFrame, lines)
+            if left:
+                cv2.putText(coppiedFrame, '<---', (0, 30), font, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
+            elif right:
+                cv2.putText(coppiedFrame, '--->', (0, 30), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+            else:
+                cv2.putText(coppiedFrame, '^', (0, 30), font, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
+            timeText = time.strftime("%Y/%m/%d %H:%M:%S %Z", time.localtime())
+            cv2.putText(coppiedFrame, timeText, (250, 20), font, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+            lineImage = displayLines(coppiedFrame, averagedLines)
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(coppiedFrame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+        k = cv2.waitKey(1)
+        if k == ord('s'):
+            break
+        elif k == ord('a'):
+            vChange += 1
+        elif k == ord('d'):
+            vChange += 1
+        cv2.imshow("origFrame", origFrame)
+        cv2.imshow("coppiedFrameWithLines", coppiedFrame)
+    timeSaved = time.strftime("%Y%m%d%H%M%S%Z", time.localtime())
+    name1 = 'detected' + timeSaved + '.csv'
+    name2 = 'detected' + timeSaved + 'HeadAndIndexFalse.csv'
+    detected.to_csv(name1)
+    detected.to_csv(name2, header=False, index=False)
+    cv2.destroyAllWindows()
