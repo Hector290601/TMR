6cade41b4a77aece0a62bc41314a534dc41d9569
HectorRobles <robletes062901@gmail.com>
2020-12-21 09:14:34 -0600

    catkin_ws

---
+++ Hector/ros/noetic/catkin_ws/build/cv_basics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hector/TMR/Hector/ros/noetic/catkin_ws/src/cv_basics
+# Build directory: /home/hector/TMR/Hector/ros/noetic/catkin_ws/build/cv_basics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
