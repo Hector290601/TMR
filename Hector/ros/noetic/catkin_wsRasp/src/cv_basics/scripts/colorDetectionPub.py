de2c3399f19ecac4dca43fb1bae6b1bf7d2d442e
Héctor290601 <robletes062901@gmail.com>
2021-07-19 18:10:10 +0000

    AutoModelCarRulebook.pdf

--- Hector/ros/noetic/catkin_wsRasp/src/cv_basics/scripts/colorDetectionPub.py
+++ Hector/ros/noetic/catkin_wsRasp/src/cv_basics/scripts/colorDetectionPub.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
+import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
@@ -9,8 +10,10 @@ def publishImage(img):
     pub = rospy.Publisher('videoFramecolorDetection', Image, queue_size=10)
     br = CvBridge()
     frame = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    kernel = np.ones((15, 15), np.uint8)
+    eroded = cv2.erode(frame, kernel, iterations = 1)
     rospy.loginfo('Publicando video en colorDetection')
-    pub.publish(br.cv2_to_imgmsg(frame))
+    pub.publish(br.cv2_to_imgmsg(eroded))
 
 def callback(data):
     br = CvBridge()
