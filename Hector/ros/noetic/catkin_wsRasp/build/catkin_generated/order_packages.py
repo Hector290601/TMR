698f2e182bc7fafb4aa25cf3b8bc3a43ffbaa51c
Héctor290601 <robletes062901@gmail.com>
2022-01-23 22:33:17 +0000

    AutoModelCarRulebook.pdf

--- Hector/ros/noetic/catkin_wsRasp/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ubuntu/TMR/Hector/ros/noetic/catkin_wsRasp/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ubuntu/TMR/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu/TMR/catkin_ws/devel;/opt/ros/noetic' != '' else []
