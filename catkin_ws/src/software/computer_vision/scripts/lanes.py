c869a8f7c1c9538740e0670c5a87f07776ed98ee
Héctor290601 <robletes062901@gmail.com>
2022-01-23 03:56:18 +0000

    AutoModelCarRulebook.pdf

--- catkin_ws/src/software/computer_vision/scripts/lanes.py
+++ catkin_ws/src/software/computer_vision/scripts/lanes.py
@@ -3,9 +3,13 @@
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
 import cv2
 import numpy as np
 import math
+
+lanes_to_publish = ""
 
 def canny_frame(frame_gray):
     blured_frame = cv2.GaussianBlur(frame_gray, (5, 5), 0)
@@ -44,6 +48,7 @@ def color_seg(frame_color, frame_gray, frame_interest):
     return ranged_frame
 
 def callback_raw_image(data):
+    global lanes_to_publish
     brdg = CvBridge()
     raw_frame = brdg.imgmsg_to_cv2(data)
     coppied_frame = np.copy(raw_frame)
@@ -51,16 +56,28 @@ def callback_raw_image(data):
     cannied_frame = canny_frame(gray_frame) #10
     interest_frame = crop_frame(cannied_frame) #15
     color_frame = color_seg(coppied_frame, gray_frame, interest_frame) #31
-    possible_lines_p = cv2.HoughLines(color_frame, 1, np.pi/180, 50, 
+    possible_lines_p = cv2.HoughLinesP(color_frame, 1, np.pi/180, 50, 
             minLineLength=20, maxLineGap=50)
     possible_lines = cv2.HoughLines(color_frame, 1, np.pi/180, 50)
-    cv2.imshow("interest_frame", interest_frame)
+    lines = []
+    for line in possible_lines:
+        for points in line:
+            for number in points:
+                lines.append(number)
+    lanes_to_publish = np.array(lines, dtype=np.float32)
+    cv2.imshow("interest_frame", coppied_frame)
     cv2.waitKey(1)
 
 def main():
+    global lanes_to_publish
     rospy.init_node('raw_img_subscriber', anonymous = True)
     rospy.Subscriber('/raw_image', Image, callback_raw_image)
-    rospy.spin()
+    lanes_publisher = rospy.Publisher("/raw_lanes", numpy_msg(Floats), queue_size=10)
+    loop = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        lanes_publisher.publish(lanes_to_publish)
+        loop.sleep()
+
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
