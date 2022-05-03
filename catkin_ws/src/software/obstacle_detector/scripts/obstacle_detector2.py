570bd2fceb8e23cfd980291615f968187ad276ff
melaniaromero <melaniapedraza17@gmail.com>
2022-05-02 19:14:12 -0500

    nuevo

--- catkin_ws/src/software/obstacle_detector/scripts/obstacle_detector2.py
+++ catkin_ws/src/software/obstacle_detector/scripts/obstacle_detector2.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
 
-def callback_Laser2(msg):
-    #si la distancia a un obstaculo en frente es mayor de 1 metro, el robot se mueve enfrente
-    if msg.ranges[0]>1:
-        move.linear.x = 0.5
-        move.angular.z =0.0
-
-     #si la distancia es mnor a 1 metro, te paras
-    print "No. of Ranges", len (msg.ranges)
-    print "Reading at position 0:", msg.ranges [0]
-
-    if msg.ranges[0]<1:
-	move.linear.x = 0.0
-	move.angular.z = 0.0
-    pub.publish(move)
+def callback_Laser2(data):
+    longitud= len(data.ranges)/2
+    #print(longitud)#363
+    #hayamos la mitad del arreglo
+    #ahora calculamos y casteamos par aobtener el numero de lecturas de 363 hacia los lados
+    lecturas= int(0.5/data.angle_increment)#81
+    #print(lecturas)
+    left= longitud-lecturas
+    right=longitud+lecturas
+    #print(left)#282
+    #print(right)#444
+    noTope=True
+    #while noTope:
+    for i in range(left,right,lecturas):#start,stop,step
+        if data.ranges[i]>1:
+            noTope=False
+            
+        else:
+            print(data.ranges[i])
 
 def listener():
-
     rospy.init_node ('software_obstacle_detector', anonymous=True)
     rospy.Subscriber("/scan", LaserScan, callback_Laser2)
-    pub = rospy.Publisher ('/cmd_vel', Twist)
     rospy.spin()
+
 def main():
-    listener()
     print("Initializing node.....")
+    listener()
 
 main()
