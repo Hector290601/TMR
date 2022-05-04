dba37028faa811faaf84acd24abde90df87fdc52
melaniaromero <melaniapedraza17@gmail.com>
2022-05-03 20:29:26 -0500

    Decision

--- catkin_ws/src/software/obstacle_detector/scripts/obstacle_detector2.py
+++ catkin_ws/src/software/obstacle_detector/scripts/obstacle_detector2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import numpy as np
 from sensor_msgs.msg import LaserScan
 
 def callback_Laser2(data):
@@ -9,18 +10,38 @@ def callback_Laser2(data):
     #ahora calculamos y casteamos par aobtener el numero de lecturas de 363 hacia los lados
     lecturas= int(0.5/data.angle_increment)#81
     #print(lecturas)
+    
     left= longitud-lecturas
     right=longitud+lecturas
+    
     #print(left)#282
     #print(right)#444
+    
     noTope=True
-    #while noTope:
-    for i in range(left,right,lecturas):#start,stop,step
-        if data.ranges[i]>1:
-            noTope=False
-            
-        else:
-            print(data.ranges[i])
+
+   
+    #interes=np.array(data.ranges[left:right]).astype(int) #recorta solo el interes, convierte arreglo a entero     
+    
+    #interes = str(interes)[1:-1]#quitar losparentesis 
+    
+    
+   
+    #voto= interes.find("0")
+    voto= 0
+    for i in data.ranges[left:right]: 
+        if i < 1:
+            voto +=1
+
+    if voto > 40:
+        print("Estorba")
+        noTope=True
+    else:
+        noTope=False
+        print("Todo libre")
+    #print(interes.find("0 0 0"))
+    #print(voto)
+    #print(interes)
+
 
 def listener():
     rospy.init_node ('software_obstacle_detector', anonymous=True)
