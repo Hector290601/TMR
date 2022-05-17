278da639ce292f794f2235922fe5bd0b2afbc424
melaniaromero <melaniapedraza17@gmail.com>
2022-05-16 20:00:01 -0500

    Obstaculos con nubes de puntos

--- catkin_ws/src/software/obstacle_detector/scripts/obstacle_detector3.py
+++ catkin_ws/src/software/obstacle_detector/scripts/obstacle_detector3.py
@@ -3,12 +3,32 @@ import rospy
 import numpy as np
 from sensor_msgs.msg import PointCloud2
 from std_msgs.msg import Bool
-import ros_numpy
+from sensor_msgs import point_cloud2
 
-def callback_cloud(msg):
-    print(msg.width)
-    #arr = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
-    #print(arr[10000])
+
+def callback_cloud(data):
+    assert isinstance(data, PointCloud2)
+    gen=point_cloud2.read_points(data,field_names=("x","y","z"),skip_nans=True)  
+    #print type(gen)
+    noTope=True
+    voto=0
+    #><
+    for p in gen:
+        if -3<p[0]<3 and -1.5<p[1]<0.5 and -20<p[2]<-2:
+	    voto +=1
+    noTope=voto<40
+    print(("Todo libre" if noTope else "Estorba"))
+    """if voto> 40:
+            print("Estorba")
+            noTope=True
+	else:
+	   noTope=False
+	   print("Todo libre")
+            
+	#print "x : %.3f y:%.3f z:%.3f" %(p[0],p[1],p[2])
+        #print(msg.width)
+	#arr = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
+    """
 
 def main():
     print("Initializing node.....")
