bc8af43165f5522669044be94c6bf1cdf389a5c1
Hector290601 <robgletes062901@gmail.com>
2022-08-13 14:04:01 -0500

    some changes

--- catkin_ws/src/hardware/mobile_base/src/webCamPub.cpp
+++ catkin_ws/src/hardware/mobile_base/src/webCamPub.cpp
@@ -9,18 +9,29 @@ int main(int argc, char** argv){
 	ros::NodeHandle nh;
 	int camera_index = 0;
 	cv::VideoCapture capture(camera_index);
-		/*
 	if(!capture.isOpened()){
+		std::cout<<"Failed to open camera"<<std::endl;
 		ROS_ERROR_STREAM("Failed to open camera" << camera_index << "!");
 		ros::shutdown();
 	}
+	std::cout<<"Success to open camera"<<std::endl;
 	image_transport::ImageTransport it(nh);
 	image_transport::Publisher pubFrame = it.advertise("camera", 1);
 	cv::Mat frame;
 	sensor_msgs::ImagePtr msg;
-	ros::Rate loopRate(60);
+	ros::Rate loopRate(10);
 	while(nh.ok()){
-		capture >> frame;
+		try{
+			capture >> frame;
+		std::cout<<"loop"<<std::endl;
+		}catch(cv::Exception ex){
+			std::cout<<ex.what()<<std::endl;
+			ROS_ERROR_STREAM(ex.what());
+		}catch(...){
+			std::cout<<"Unknow error"<<std::endl;
+			ROS_ERROR_STREAM("Unknow error");
+		}
+		/*
 		if(frame.empty()){
 			ROS_ERROR_STREAM("Failed to capture image from camera " << camera_index << "!");
 			ros::shutdown();
@@ -28,9 +39,10 @@ int main(int argc, char** argv){
 		msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
 		pubFrame.publish(msg);
 		cv::waitKey(1);
+		*/
+		std::cout<<"loop"<<std::endl;
 		ros::spinOnce();
 		loopRate.sleep();
 	}
-		*/
 	capture.release();
 }
