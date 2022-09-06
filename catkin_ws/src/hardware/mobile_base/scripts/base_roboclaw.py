51837b9b0674470ab8c23910934208e3605776f8
Héctor Robles <robletes062901@gmail.com>
2022-09-06 16:44:14 -0500

    first roboclaw base node draft

--- catkin_ws/src/hardware/mobile_base/scripts/base_roboclaw.py
+++ catkin_ws/src/hardware/mobile_base/scripts/base_roboclaw.py
@@ -24,7 +24,7 @@ def callback_steering(msg):
 
 def callback_speed(msg):
     global speed
-    speed = msg.data #Speed in PWM
+    speed = msg.data #Speed in percentage
 
 def main():
     global steering, speed
@@ -34,30 +34,19 @@ def main():
     rospy.Subscriber("/steering", Float32, callback_steering)
     rospy.Subscriber("/speed", Float32, callback_speed)
     loop = rospy.Rate(60)
-    servoPin = 12
-    motorPin = 16
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(servoPin,GPIO.OUT)
-    GPIO.setup(motorPin,GPIO.OUT)
     steering = 0
     speed = 0
-    servo = GPIO.PWM(servoPin, 100)
-    servo.start(5.0)
-    motor = GPIO.PWM(motorPin, 60.2)
-    motor.start(0)
     cap = cv2.VideoCapture(0)
     brdg = CvBridge()
+    main_motor = Roboclaw("/dev/ttyACM0", 115200)
+    if main_motor == 0:
+        return False
     print("ALL SUCCESFULLY INITIALIZED")
     dutySpeed, dutySteering=AngleToDuty(8.5, 0)
-    servo.ChangeDutyCycle(dutySteering)
-    motor.ChangeDutyCycle(dutySpeed)
     dutySpeed, dutySteering=AngleToDuty(0, 0)
-    servo.ChangeDutyCycle(dutySteering)
-    motor.ChangeDutyCycle(dutySpeed)
     while not rospy.is_shutdown():
         dutySpeed, dutySteering=AngleToDuty(speed, steering)
-        servo.ChangeDutyCycle(dutySteering)
-        motor.ChangeDutyCycle(dutySpeed)
+        main_motor.ForwardM1(0x80, speed)
         ret, frame = cap.read()
         if ret == True:
             img_publisher.publish(brdg.cv2_to_imgmsg(frame))
