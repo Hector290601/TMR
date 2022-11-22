ce40ecfedbdf49b60beae6cbb965c098282cd156
Héctor Robles <robletes062901@gmail.com>
2022-11-22 15:03:20 -0600

    test
    added simple test and ranges to the sender script :)

--- Hector/arduino/timer_interrupt/sender.py
+++ Hector/arduino/timer_interrupt/sender.py
@@ -1,10 +1,26 @@
 import serial
+import time
 
 arduino = serial.Serial("COM5", 115200)
 
 while True:
-    for i in range(32, 126):
+    """
+    i = int(input("32~127: "))
+    arduino.write(chr(i).encode())
+    """
+    delay = 0.1
+    for i in range(95, 125):
         print(i, end=":")
         print(chr(i))
+        time.sleep(delay)
         arduino.write(chr(i).encode())
-        input()
+    for i in range(125, 75, -1):
+        print(i, end=":")
+        print(chr(i))
+        time.sleep(delay)
+        arduino.write(chr(i).encode())
+    for i in range(75, 95):
+        print(i, end=":")
+        print(chr(i))
+        time.sleep(delay)
+        arduino.write(chr(i).encode())
