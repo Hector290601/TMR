a5dbb8b32466898fec1ec1f93f1546f4d1077c7f
Hector Robles <robletes062901@gmail.com>
2023-03-28 18:32:03 -0600

    Stop detector. Improved stop detector script to filter detected shapes by area lenght and encloses it to a circle.

--- catkin_ws/src/software/computer_vision/scripts/stop_detector.py
+++ catkin_ws/src/software/computer_vision/scripts/stop_detector.py
@@ -6,8 +6,8 @@ from cv_bridge import CvBridge
 import cv2
 import numpy as np
 
-upper_color = [175, 233, 198]
-lower_color = [171, 170, 160]
+upper_color = [124, 18, 182]
+lower_color = [81, 3, 134]
 
 def callback_raw_image(data):
     global upper_color, lower_color
@@ -22,10 +22,21 @@ def callback_raw_image(data):
     imgray = cv2.cvtColor(img_dilation, cv2.COLOR_BGR2GRAY)
     thresh = cv2.adaptiveThreshold(imgray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
     im2, contours, hierarchy= cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame, contours, -1, (0,255,0), 3)
-    #print(contours)
+    for cnt in contours:
+        area = cv2.contourArea(cnt) 
+        if area > 30000 and area < 60000:
+            M = cv2.moments(cnt)
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            frame = cv2.circle(frame, (cx, cy), 20, (255, 255, 255))
+            print(cv2.contourArea(cnt))
+            approx = cv2.convexHull(cnt)
+            (x,y),radius = cv2.minEnclosingCircle(cnt)
+            center = (int(x),int(y))
+            radius = int(radius)
+            cv2.circle(frame, center, radius, (0, 0, 255), 2)
+            cv2.drawContours(frame, cnt, -1, (0,255,0), 3)
     cv2.imshow('mask', thresh)
-    #cv2.imshow('result', result)
     cv2.imshow('original', frame)
     cv2.waitKey(1)
 
