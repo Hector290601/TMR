a37d024f83a49c0c1a20097469291b10b7978e61
Hector290601 <robletes062901@gmail.com>
2023-04-17 17:11:33 -0500

    Fixed color selector This version fixes the last version problem about the core dumped

--- catkin_ws/src/software/computer_vision/scripts/color_selector.py
+++ catkin_ws/src/software/computer_vision/scripts/color_selector.py
@@ -9,7 +9,7 @@ import numpy as np
 frame = None
 
 upper_color = [0, 0, 0]
-lower_color = [179, 255, 255]
+lower_color = [178, 255, 255]
 
 def mouse_click(event, x, y, 
                 flags, param):
@@ -20,22 +20,22 @@ def mouse_click(event, x, y,
         lh, ls, lv = lower_color
         print(upper_color)
         if h > uh:
-            upper_color[0] = h
+            upper_color[0] = int(h)
             print("Upper H channel changed from {} to {}".format(uh, h))
         elif h < lh:
-            lower_color[0] = h
+            lower_color[0] = int(h)
             print("Lower H channel changed from {} to {}".format(lh, h))
         if s > us:
-            upper_color[1] = s
+            upper_color[1] = int(s)
             print("Upper S channel changed from {} to {}".format(us, s))
         elif s < ls:
-            lower_color[1] = s
+            lower_color[1] = int(s)
             print("Lower S channel changed from {} to {}".format(ls, s))
         if v > uv:
-            upper_color[2] = v
+            upper_color[2] = int(v)
             print("Upper V channel changed from {} to {}".format(uv, v))
         elif v < lv:
-            lower_color[2] = v
+            lower_color[2] = int(v)
             print("Lower V channel changed from {} to {}".format(lv, v))
     if event == cv2.EVENT_RBUTTONDOWN:
         upper_color = [0, 0, 0]
@@ -46,26 +46,17 @@ def callback_raw_image(data):
     global upper_color, lower_color
     brdg = CvBridge()
     frame = brdg.imgmsg_to_cv2(data)
-<<<<<<< HEAD
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
-    mask = cv2.inRange(hsv, np.array(lower_color), np.array(upper_color))
-=======
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #print([lower_color, upper_color])
-    cv2.imshow("hsv", hsv)
-    return
-    mask = cv2.inRange(hsv, (lower_color[0], lower_color[1], lower_color[2]), (upper_color[0], upper_color[1], upper_color[2]))
->>>>>>> 715415fbc217cc2ce7db658aa3784d0e9d154d93
+    mask = cv2.inRange(hsv, np.array(lower_color), np.array(upper_color))
     result = cv2.bitwise_and(frame, frame, mask = mask)
-    #cv2.imshow('mask', mask)
+    cv2.imshow("hsv", hsv)
     cv2.imshow('result', result)
     cv2.imshow('original', frame)
-    #cv2.setMouseCallback('camera', mouse_click)
-    #cv2.setMouseCallback('mask', mouse_click)
+    cv2.setMouseCallback('result', mouse_click)
+    cv2.setMouseCallback('original', mouse_click)
+    cv2.setMouseCallback('hsv', mouse_click)
     k = cv2.waitKey(1)
-    if k == ord('q'):
-        exit()
-    elif k == ord('p'):
+    if k == ord('p'):
         uh, us, uv = upper_color
         lh, ls, lv = lower_color
         print("lower_color = [{}, {}, {}] \nupper_color = [{}, {}, {}]".format(
@@ -77,16 +68,7 @@ def callback_raw_image(data):
 def main():
     rospy.init_node('raw_img_subscriber', anonymous = True)
     rospy.Subscriber('/raw_image', Image, callback_raw_image)
-    cv2.namedWindow("result")
-    cv2.namedWindow("original")
-    cv2.namedWindow("hsv")
-    cv2.setMouseCallback('result', mouse_click)
-    #cv2.setMouseCallback('original', mouse_click)
-    #cv2.setMouseCallback('hsv', mouse_click)
     rospy.spin()
-    #loop = rospy.Rate(30)
-    #while not rospy.is_shutdown() and cv2.waitKey(10):
-    #    loop.sleep()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
