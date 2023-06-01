716a80321af1a1587abc3ef2a0b4ee63dce66721
Hector Robles <robletes062901@gmail.com>
2023-06-01 12:19:22 -0600

    changes on servo controller

--- arduino/arduino_ros_servo/arduino_ros_servo.ino
+++ arduino/arduino_ros_servo/arduino_ros_servo.ino
@@ -3,6 +3,23 @@
 #include <Servo.h> //import pwm generator lib.
 ros::NodeHandle  nh; //declare node handler
 Servo servo; //declare servo
+unsigned long start = 0.0;
+
+void blink_callback( const std_msgs::Float64& msg){
+  float blinking = msg.data;
+  if(blinking > 0.0){
+    digitalWrite(6, 0);
+    if(millis() >  (start + 250.0)){
+      digitalWrite(4, 1);
+      start = millis();
+    }else{
+      digitalWrite(4, 0);
+    }
+  }else{
+    digitalWrite(4, 0);
+    digitalWrite(6, 0);
+  }
+}
 
 void messageCb( const std_msgs::Float64& msg){ //msg callback
   float radian = msg.data; //load msg value to a float type variable
@@ -17,12 +34,17 @@ void messageCb( const std_msgs::Float64& msg){ //msg callback
 }
 
 ros::Subscriber<std_msgs::Float64> sub("/steering", messageCb ); //configure servo callback
+ros::Subscriber<std_msgs::Float64> sub_blinking("/blinking", blink_callback ); //configure servo callback
 
 void setup(){
   servo.attach(2); //pwm channel output to the pin 2
   servo.write(90); //initial pwm value
+  pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
   nh.initNode(); //ros init node
   nh.subscribe(sub); //ros subscriber
+  nh.subscribe(sub_blinking); //ros subscriber
+  start = millis();
 }
 
 void loop(){
